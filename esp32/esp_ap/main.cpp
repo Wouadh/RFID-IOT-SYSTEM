a883782146eac93a619d7613c7495bd5e13aad91
Wouadh <153502217+Wouadh@users.noreply.github.com>
2026-01-12 20:56:24 +0100

    Update main.cpp

--- esp32/esp_ap/main.cpp
+++ esp32/esp_ap/main.cpp
@@ -1,26 +1,27 @@
 #include <WiFi.h>
 
-/* ===== Configuration AP ===== */
-const char* ssid = "ESP32_RFID_AP";
-const char* password = "12345678";   // minimum 8 caractères
+
+const char* ssid = "ESP32_RFID_AP"; // nom d utilisateur  
+const char* password = "12345678";   // mot de passe du wifi
 
 IPAddress local_IP(192, 168, 4, 1);   // IP de l’ESP32 AP
-IPAddress gateway(192, 168, 4, 1);
-IPAddress subnet(255, 255, 255, 0);
+IPAddress gateway(192, 168, 4, 1);    // marquer l esp AP comme gateway pour gerer tout le reseau
+IPAddress subnet(255, 255, 255, 0); // masque de reseau (jusqua 254 appareils sur le resau)
 
 void setup() {
   Serial.begin(115200);
 
-  // Configure ESP32 as Access Point only
+  // Configurer l ESP32 comme point d acces
   WiFi.mode(WIFI_AP);
-  WiFi.softAPConfig(local_IP, gateway, subnet);
+  WiFi.softAPConfig(local_IP, gateway, subnet); //l'ESP32 prendra lIP par défaut
   WiFi.softAP(ssid, password);
-
+// affichage de l adresse IP
   Serial.println("ESP32 Access Point started");
   Serial.print("AP IP address: ");
   Serial.println(WiFi.softAPIP());
 }
 
 void loop() {
-  // Nothing to do here
+  
 }
+
